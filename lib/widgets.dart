0ecf5572b441b6dba7110a55934489c8fa599422
Kamil Rykowski <kamil@lab4.life>
2020-08-11 07:27:56 +0200

    Update load image signature #15

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -166,6 +166,7 @@ class ScratcherState extends State<Scratcher> {
       Uint8List bytes, {
       int cacheWidth,
       int cacheHeight,
+      bool allowUpscaling,
     }) async {
       return await ui.instantiateImageCodec(bytes);
     })
